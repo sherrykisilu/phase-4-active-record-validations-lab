97a55b664af0c90330a70be4990d3bec44ab6ee4
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-05-19 12:26:42 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :sufficiently_clickbait_title
+
+  def sufficiently_clickbait_title
+    unless title&.match?(/Won't Believe|Secret|Top \d+|Guess/)
+      errors.add(:title, "should be sufficiently clickbait-y")
+    end
+  end
+  end
+  
